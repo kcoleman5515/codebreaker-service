c9fb68cd9516c95d8ec3550e557814a45d73dd55
Kyle Coleman <kcoleman20@cnm.edu>
2022-03-09 11:57:38 -0700

    Guess validation complete, processing in progress

---
+++ src/main/java/edu/cnm/deepdive/codebreaker/model/dao/GuessRepository.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.codebreaker.model.dao;
+
+import edu.cnm.deepdive.codebreaker.model.entity.Game;
+import edu.cnm.deepdive.codebreaker.model.entity.Guess;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface GuessRepository extends JpaRepository<Guess, UUID> {
+
+  Optional<Guess> findByGameAndExternalKey(Game game, UUID externalKey);
+
+}
