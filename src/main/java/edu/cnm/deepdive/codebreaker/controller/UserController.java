36bc4ad4ce94c0858b57255880bf07271fa8debb
Kyle Coleman <kcoleman20@cnm.edu>
2022-03-21 12:05:49 -0600

    Performance statistics done.

--- src/main/java/edu/cnm/deepdive/codebreaker/controller/UserController.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/controller/UserController.java
@@ -1,7 +1,9 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import edu.cnm.deepdive.codebreaker.model.entity.User;
 import edu.cnm.deepdive.codebreaker.service.AbstractUserService;
+import edu.cnm.deepdive.codebreaker.view.UserView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,14 +24,33 @@ public class UserController {
   }
 
   @GetMapping(value = "/me", produces = MediaType.APPLICATION_JSON_VALUE)
+  @JsonView(UserView.Private.class)
   public User get() {
     return service.getCurrentUser();
   }
 
   @PutMapping(value = "/me",
       consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  @JsonView(UserView.Private.class)
   public User put(@RequestBody User user) {
     return service.updateUser(user);
+  }
+
+  @GetMapping(value = "/me/incognito", produces = MediaType.APPLICATION_JSON_VALUE)
+  public boolean isIncognito() {
+    return service
+        .getCurrentUser()
+        .isIncognito();
+  }
+
+  @PutMapping(value = "/me/incognito",
+      consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public boolean setIncognito(@RequestBody boolean incognito) {
+    User user = service.getCurrentUser();
+    user.setIncognito(incognito);
+    return service
+        .updateUser(user)
+        .isIncognito();
   }
 
 }
--- src/main/java/edu/cnm/deepdive/codebreaker/service/PerformanceService.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/service/PerformanceService.java
@@ -1,7 +1,9 @@
 package edu.cnm.deepdive.codebreaker.service;
 
 import edu.cnm.deepdive.codebreaker.model.dao.GamePerformanceRepository;
+import edu.cnm.deepdive.codebreaker.model.dao.UserPerformanceRepository;
 import edu.cnm.deepdive.codebreaker.model.view.GamePerformance;
+import edu.cnm.deepdive.codebreaker.model.view.UserPerformance;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -10,24 +12,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class PerformanceService implements AbstractPerformanceService {
 
-  private final GamePerformanceRepository repository;
+  private final GamePerformanceRepository gamePerformanceRepository;
+  private final UserPerformanceRepository userPerformanceRepository;
 
-  public PerformanceService(GamePerformanceRepository repository) {
-    this.repository = repository;
+  public PerformanceService(GamePerformanceRepository gamePerformanceRepository,
+      UserPerformanceRepository userPerformanceRepository) {
+    this.gamePerformanceRepository = gamePerformanceRepository;
+    this.userPerformanceRepository = userPerformanceRepository;
   }
 
   @Override
-  public Iterable<GamePerformance> getGameRankingsByDuration(int poolSize, int codeLength, int count) {
+  public Iterable<GamePerformance> getGameRankingsByDuration(int poolSize, int codeLength,
+      int count) {
     Sort sort = Sort
         .by("duration")
         .ascending()
         .and(
-          Sort
-              .by("guessCount")
-              .ascending()
+            Sort
+                .by("guessCount")
+                .ascending()
         );
     Pageable pageable = PageRequest.of(0, count, sort);
-    return repository.findAllByPoolSizeAndLength(poolSize, codeLength, pageable);
+    return gamePerformanceRepository.findAllByPoolSizeAndLength(poolSize, codeLength, pageable);
   }
 
   @Override
@@ -42,6 +48,38 @@ public class PerformanceService implements AbstractPerformanceService {
                 .ascending()
         );
     Pageable pageable = PageRequest.of(0, count, sort);
-    return repository.findAllByPoolSizeAndLength(poolSize, codeLength, pageable);
+    return gamePerformanceRepository.findAllByPoolSizeAndLength(poolSize, codeLength, pageable);
+  }
+
+  @Override
+  public Iterable<UserPerformance> getUserRankingsByDuration(int poolSize, int codeLength,
+      int minGamesCompleted, int count) {
+    Sort sort = Sort
+        .by("averageDuration")
+        .ascending()
+        .and(
+            Sort
+                .by("averageGuessCount")
+                .ascending()
+        );
+    Pageable pageable = PageRequest.of(0, count, sort);
+    return userPerformanceRepository.findAllByPoolSizeAndLengthAndGamesCompletedGreaterThanEqual(
+        poolSize, codeLength, minGamesCompleted, pageable);
+  }
+
+  @Override
+  public Iterable<UserPerformance> getUserRankingsByGuessCount(int poolSize, int codeLength,
+      int minGamesCompleted, int count) {
+    Sort sort = Sort
+        .by("averageGuessCount")
+        .ascending()
+        .and(
+            Sort
+                .by("averageDuration")
+                .ascending()
+        );
+    Pageable pageable = PageRequest.of(0, count, sort);
+    return userPerformanceRepository.findAllByPoolSizeAndLengthAndGamesCompletedGreaterThanEqual(
+        poolSize, codeLength, minGamesCompleted, pageable);
   }
 }
